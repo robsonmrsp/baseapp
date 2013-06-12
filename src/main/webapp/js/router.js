bf8b99202cf5a3738a95ab8cc2bcc3f31788bd18
robsonmrsp <robsonmrsp@gmail.com>
2013-06-12 14:27:17 -0300

    ajuste

--- src/main/webapp/js/router.js
+++ src/main/webapp/js/router.js
@@ -5,35 +5,34 @@ define([ 'jquery', 'underscore', 'backbone', 'views/user/UsersPage', 'models/Use
 			'app/users' : 'users',
 			'app/someEntity' : 'someEntity',
 		},
-
-		initialize : function() {
-			this.on('route', this.changeHash, this);
-		},
 	});
 
+	var loadUser = function() {
+		var loggedUser = new UserModel();
+		loggedUser.urlRoot = 'rs/crud/users/loggedUser';
+		loggedUser.fetch({
+			success : function(_user, _resp, _options) {
+				if (_user) {
+					$('#menuUserPhoto').attr('src', _user.get('photo'));
+					$('#menuUserName').html(_user.get('username'));
+					$('#menuUserRole').html(_user.get('role').description);
+				}
+			},
+			error : function(_model, _xhr, _options) {
+				console.log('Erro');
+			}
+		});
+	}
+	
 	var initialize = function() {
 
 		var appRouter = new AppRouter;
-		var that = this;
 		appRouter.on('route:index', function() {
-			var loggedUser = new UserModel();
-			loggedUser.urlRoot = 'rs/crud/users/loggedUser';
-			loggedUser.fetch({
-				success : function(_user, _resp, _options) {
-					if (_user) {
-						$('#menuUserPhoto').attr('src', _user.get('photo'));
-						$('#menuUserName').html(_user.get('username'));
-						$('#menuUserRole').html(_user.get('role').description);
-					}
-				},
-
-				error : function(_model, _xhr, _options) {
-					console.log('Erro');
-				}
-			});
+			loadUser();
 		});
 
 		appRouter.on('route:users', function() {
+			loadUser()
 			var pageUsers = new PageUsers();
 			pageUsers.render();
 		});
